21711fcc108e0d6cec545bbb8c919e7a3a92b78a
KIMURA Tetsuro <haribote.nobody@gmail.com>
2017-11-27 23:40:49 +0900

    ベースのスタイルを書いた。

--- src/components/App/app.vue
+++ src/components/App/app.vue
@@ -1,7 +1,28 @@
 <template>
   <div id="app">
-    <img src="../../assets/logo.png">
-    <HelloWorld/>
+    <header class="container">
+      <h1>Vue Graph Demo</h1>
+      <p>Demos of graph that render by Vue.js without D3.js.</p>
+    </header>
+
+    <div class="container">
+      <h2>Line chart</h2>
+      <p>Under constructing...</p>
+    </div>
+
+    <div class="container">
+      <h2>Bar chart</h2>
+      <p>Under constructing...</p>
+    </div>
+
+    <div class="container">
+      <h2>Pie chart</h2>
+      <p>Under constructing...</p>
+    </div>
+
+    <footer class="container">
+      <p><a href="https://github.com/haribote/vue-graph-demo" target="_blank">Fork on GitHub</a></p>
+    </footer>
   </div>
 </template>
 
