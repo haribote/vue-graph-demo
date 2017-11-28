002eda2557bbdf646f282003c09c1cc9f975b50f
KIMURA Tetsuro <haribote.nobody@gmail.com>
2017-11-29 02:47:20 +0900

    Appコンポーネントに必要なデータをフェッチする機能を実装した。

--- src/components/App/app.spec.ts
+++ src/components/App/app.spec.ts
@@ -1,13 +1,30 @@
 import { expect } from 'chai'
+import { useFakeXMLHttpRequest, spy } from 'sinon'
 import { shallow } from 'vue-test-utils'
 
 import App from './app.vue'
 
+import { API_NPB_LEAGUES, API_NPB_TEAMS, API_NUMBER_OF_VISITORS_HISTORY, API_PENNANT_RACE_HISTORY } from '../../api/endpoints'
+
 describe('App.vue', () => {
   let wrapper
+  let xhr
+  let requests
 
   beforeEach(() => {
     wrapper = shallow(App)
+  })
+
+  before(() => {
+    xhr = useFakeXMLHttpRequest()
+    requests = []
+    xhr.onCreate = (req) => {
+      requests.push(req)
+    }
+  })
+
+  after(() => {
+    xhr.restore()
   })
 
   it('should render app wrapper', () => {
@@ -37,5 +54,12 @@ describe('App.vue', () => {
     /* tslint:disable-next-line:no-unused-expression */
     expect(containerElList.at(4).is('footer'))
       .to.be.true
+  })
+
+  it('should request data', () => {
+    expect(requests[0].url).to.equal(API_NPB_LEAGUES)
+    expect(requests[1].url).to.equal(API_NPB_TEAMS)
+    expect(requests[2].url).to.equal(API_NUMBER_OF_VISITORS_HISTORY)
+    expect(requests[3].url).to.equal(API_PENNANT_RACE_HISTORY)
   })
 })
--- src/components/App/app.ts
+++ src/components/App/app.ts
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import 'core-js/fn/promise'
+
+import { fetchNpbLeagues, fetchNpbTeams, fetchNpbNumberOfVisitorsHistory, fetchNpbPennantRaceHistory } from '../../api/driver'
 
 import TabList from '../TabList/tab-list.vue'
 
@@ -11,14 +14,35 @@ export default Vue.extend({
 
   data () {
     return {
-      lineChartTabLabels: ['Pacific League', 'Central League']
+      lineChartTabLabels: []
     }
   },
 
   methods: {
+    fetchAll () {
+      return Promise.all([
+        fetchNpbLeagues(),
+        fetchNpbTeams(),
+        fetchNpbNumberOfVisitorsHistory(),
+        fetchNpbPennantRaceHistory()
+      ])
+    },
+
     handleClickLineChartTabItem (index: number) {
       // TODO: 本実装時にテストを書く
       console.log(index)
     }
+  },
+
+  created () {
+    this.fetchAll()
+      .then(resps => {
+        const [leagues] = resps
+
+        Object.assign(this, {
+          lineChartTabLabels: leagues.map(league => league.name)
+        })
+      })
+      .catch(err => console.error(err.message))
   }
 })
