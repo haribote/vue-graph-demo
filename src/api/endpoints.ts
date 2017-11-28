089c6ecb33f74045ae70a0c24a31cb946532760c
KIMURA Tetsuro <haribote.nobody@gmail.com>
2017-11-29 00:40:46 +0900

    APIエンドポイントを定数として設定した。

---
+++ src/api/endpoints.ts
@@ -0,0 +1,5 @@
+export const API_ROOT = './static'
+export const API_NPB_LEAGUES = `${API_ROOT}/npb-leagues.json`
+export const API_NPB_TEAMS = `${API_ROOT}/npb-teams.json`
+export const API_NUMBER_OF_VISITORS_HISTORY = `${API_ROOT}/npb-number-of-visitors-history.json`
+export const API_PENNANT_RACE_HISTORY = `${API_ROOT}/npb-pennant-race-history.json`
